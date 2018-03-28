ab3ef28d100b04081f8fdaa082311b2dd0ce0a84
ahmedismail13 <ahmed150842@bue.edu.eg>
2018-03-28 19:17:13 +0200

    ismail

---
+++ js/register.js
@@ -0,0 +1,46 @@
+function validateRegisterForm() {
+    var letters =/[A-Za-z]/;
+    var number = /\d/;
+    var at = "@";
+    
+    var firstname = document.getElementById("fname").value;
+    var lastname = document.getElementById("ln").value;
+    var password = document.getElementById("pass").value; 
+    var cpassword = document.getElementById("cpass").value;
+    var email = document.getElementById("email").value;
+    var deliveryAddress = document.getElementById("daddress").value;
+    var sameAddress = document.getElementById("asDelivery");
+    var billingAddress;
+    if(sameAddress.checked)
+    {
+        billingAddress = deliveryAddress;
+        document.getElementById("baddress").readOnly= true;
+    }
+    billingAddress = document.getElementById("baddress").value;
+    var phonenumber = document.getElementById("phonenumber").value;
+    var birthdate = document.getElementById("bdate").value;
+    
+    if(!firstname.match(letters)) {
+    window.alert("First name must be letters");
+    }
+    if(!lastname.match(letters)) {
+    window.alert("Last name must be letters");
+    }
+    if(!email.match(at)) {
+    window.alert("Email must contain @");
+    }
+    if(cpassword != password) {
+    window.alert("Password do not match");
+    }     
+    if(!phonenumber.match(number))
+    {
+        window.alert("Phone number contains numbers only");
+    } else {
+    return true;
+    }
+
+
+
+    
+    
+}
