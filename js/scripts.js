a7c79c9ce76a0685f0427b133aa9c951cd1c57f9
ragrag <raggi144711@bue.edu.eg>
2018-03-30 00:27:53 +0200

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
             $('.signIn').toggle();
       if ( $('.signIn').is(':visible')){
-          $('.main-body').css("filter", "blur(10px)");}
+          $('.main-body').css("filter", "blur(10px)");
+		  }
       else{
           $('.main-body').css("filter", "blur(0px)");
       }
