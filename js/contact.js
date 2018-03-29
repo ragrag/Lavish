d679fb49a6490a209628b28045e1f51fd0886745
ragrag <raggi144711@bue.edu.eg>
2018-03-29 11:37:09 +0200

    Contact Validation

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,16 @@
 
 function validateForm(){
-    window.alert("in");
     var letters = /[A-Za-z]/;
     var dot=".";
     var at = "@";
     var name = document.getElementById("input_name").value;
     var email = document.getElementById("input_email").value;     
     var msg = document.getElementById("input_message").value;
-    if(!firstname.match(letters)) {
+    if(!name.match(letters)) {
     window.alert("Name is required.");
     }
     if(!email.match(at) ) {
-    window.alert("Valid email is required.");
+    window.alert("valid email is required.");
     }
     if(!msg.match(letters) ) {
     window.alert("Please, write us a message.");
