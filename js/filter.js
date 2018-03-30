9416f3954a830186c115e62fa8be8ccea32a0ede
nouryousry <nourhan149751@bue.edu.eg>
2018-03-30 20:59:44 +0200

    products dummy data changes

--- js/filter.js
+++ js/filter.js
@@ -1,22 +1,4 @@
-var $divs = $("div.column");
 
-$('#byName').on('click', function () {
-    var sortDivsNames = $divs.sort(function (a, b) {
-        var x=$(a).find("h6").text();
-        var y=$(b).find("h6").text();
-        return x>y;
-    });
-    $(".table").html(sortDivsNames);
-});
-
-$('#byPrice').on('click', function () {
-    var sortPriceDiv = $divs.sort(function (a, b) {
-     var x=parseInt($(a).find(".product_price").text());
-       y=parseInt($(b).find(".product_price").text()); 
-        return x > y;
-    });
-    $(".table").html(sortPriceDiv);
-});
 
 filterSelection("all");
 
@@ -25,15 +7,13 @@ function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
+function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -44,8 +24,7 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
