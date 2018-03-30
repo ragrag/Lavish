5ced0aaa83f61afc33747efda848c67dc289e4ea
ragrag <raggi144711@bue.edu.eg>
2018-03-30 14:30:08 +0200

    fixes

--- js/product.js
+++ js/product.js
@@ -148,25 +148,7 @@ jQuery(document).ready(function($)
     
  
     
-    
-  /*
-    Back to Top 
-  */
-  var btn = $('#backtotop');
-
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
-
-  btn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
-  });
-
+  
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,22 @@
           $('.main-body').css("filter", "blur(0px)");
       }
         });
+
+		  
+  /*
+    Back to Top 
+  */
+  var btn = $('#backtotop');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
