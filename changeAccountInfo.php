c810e9e65bfcd508b473897fcf23e5afe6b9ae7d
ahmedismail13 <ahmed150842@bue.edu.eg>
2018-04-19 20:37:12 +0200

    account_info done

---
+++ changeAccountInfo.php
@@ -0,0 +1,34 @@
+<?php       
+    session_start();
+    if(isset($_SESSION['user_login']))
+        include 'header_logged.php'; 
+    else include 'header_not_logged.php';
+    
+    $user = 'root';
+    $password = '';
+    $db = 'lavishdb';
+    $db = new mysqli('localhost',$user, $password,$db) or die("Unable to connect!"); 
+    $user = $_SESSION['user_login'];
+    $sql = "SELECT * FROM user WHERE U_username = '$user'";
+    $result = $db->query($sql);
+    $row = mysqli_fetch_assoc($result);
+    
+    $id = $row["U_id"];
+    
+    $newFname = $_POST["fname"];
+    $newLname = $_POST["lname"];
+    $newBdate = $_POST["bdate"];
+    $newEmail = $_POST["email"];
+    $newPass = $_POST["npass"];
+    $newPhonenumber = $_POST["phonenumber"];
+
+    $sql2 = "UPDATE user SET Fname = '$newFname', Lname ='$newLname', DOB='$newBdate', Email='$newEmail', U_password='$newPass', Mobile_number='$newPhonenumber' WHERE U_id='$id'";
+    if($db->query($sql2) === TRUE)
+    {
+        session_start();
+        $_SESSION['user_login']=$user;
+        header('Location: home.php');
+    }
+    else
+        echo "Error updating record: " . $db->error;
+?>
